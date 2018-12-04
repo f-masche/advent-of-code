c42d1d96841c5e885ae0e78c02b25d786d5e5d50
Fabian Maschewski <fabian.maschewski@kaiser-x.com>
2018-12-04 17:57:05 +0100

    Add day 4 prototype

--- 2018/04/part-a.js
+++ 2018/04/part-a.js
@@ -1,4 +1,34 @@
 const Puzzle = require('../puzzle');
+ // [1518-11-01 00:00] Guard #10 begins shift
+
+const entryRegexp = /^\[(\d+\-\d+\-\d+\s+\d+:\d+)\]\s(.+)$/
+
+function parseEntry(entry) {
+  const matches = entry.match(entryRegexp);
+  const date = matches[1].replace(' ', 'T') + ':00';
+
+  let action;
+  let guardId;
+
+  if (matches[2] === 'wakes up') {
+    action = 'wakeup';
+  } else if (matches[2] === 'falls asleep') {
+    action = 'sleep';
+  } else {
+    action = 'change';
+    guardId = matches[2].match(/^\w+\s+#(\d+)/)[1];
+  }
+
+  return {
+    date: new Date(date),
+    action,
+    guardId
+  }
+}
+
+function run(input) {
+  console.log(input.map(parseEntry))
+}
 
 const puzzle = new Puzzle('04 A');
 puzzle.addTest('input/test-a.txt', '240');
