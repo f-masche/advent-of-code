a718b98d4d9c64c22f7157b74c7085e2f53bc17f
Fabian Maschewski <f.maschewski@mail.de>
2018-12-05 08:59:06 +0100

    Refactor day 5 solution

--- 2018/05/lib.js
+++ 2018/05/lib.js
@@ -1,24 +1,19 @@
 module.exports = {
   react(polymer) {
-    let prev = [];
-    let startPointer = 0;
+    let newPolymer = [];
 
-    while(prev.length !== polymer.length) {
-      prev = polymer;
+    for (let i = 0; i < polymer.length; i++) {
+      let char = polymer[i];
+      let prev = newPolymer[newPolymer.length - 1];
 
-      for (let i = startPointer; i < polymer.length - 1; i++) {
-        let char = polymer[i];
-        let nextChar = polymer[i + 1];
-
-        if (char !== nextChar) {
-          if (char.toLowerCase() === nextChar.toLowerCase()) {
-            startPointer = Math.max(0, i - 1);
-            polymer = polymer.slice(0, i).concat(polymer.slice(i + 2));
-            break;
-          }
-        }
+      if(prev
+        && prev !== char
+        && prev.toLowerCase() === char.toLowerCase()) {
+        newPolymer.pop();
+      } else {
+        newPolymer.push(char);
       }
     }
-    return polymer;
+    return newPolymer;
   }
 }
